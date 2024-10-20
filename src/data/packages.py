09cf10e990ed62b83bed3cce1b53d1092ba44290
Gabriella Schecher <gaschecher@gmail.com>
2024-10-20 14:53:59 -0400

    organize data efficiently for retrieval based on requirements

---
+++ src/data/packages.py
@@ -0,0 +1,404 @@
+# WGUPS package data. Again, I tried to use a CSV but python was struggling to interpret the data & I'm not an expert on CSV files.
+
+packages = [
+    {
+        "Package ID": 1,
+        "Address": "195 W Oakland Ave, Salt Lake City, UT 84115",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 21,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 2,
+        "Address": "2530 S 500 E, Salt Lake City, UT 84106",
+        "Delivery Deadline": "",
+        "Weight KILO": 44,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 3,
+        "Address": "233 Canyon Rd, Salt Lake City, UT 84103",
+        "Delivery Deadline": "",
+        "Weight KILO": 2,
+        "Truck Number": "2",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 4,
+        "Address": "380 W 2880 S, Salt Lake City, UT 84115",
+        "Delivery Deadline": "",
+        "Weight KILO": 4,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 5,
+        "Address": "410 S State St, Salt Lake City, UT 84111",
+        "Delivery Deadline": "",
+        "Weight KILO": 5,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 6,
+        "Address": "3060 Lester St, West Valley City, UT 84119",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 88,
+        "Truck Number": "",
+        "Delayed": True,
+        "Delayed Until": "9:05",
+        "Group": False
+    },
+    {
+        "Package ID": 7,
+        "Address": "1330 2100 S, Salt Lake City, UT 84106",
+        "Delivery Deadline": "",
+        "Weight KILO": 8,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 8,
+        "Address": "300 State St, Salt Lake City, UT 84103",
+        "Delivery Deadline": "",
+        "Weight KILO": 9,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 9,
+        "Address": "300 State St, Salt Lake City, UT 84103",
+        "Delivery Deadline": "",
+        "Weight KILO": 2,
+        "Truck Number": "",
+        "Delayed": True,
+        "Delayed Until": "10:20",
+        "Group": False
+    },
+    {
+        "Package ID": 10,
+        "Address": "600 E 900 South, Salt Lake City, UT 84105",
+        "Delivery Deadline": "",
+        "Weight KILO": 1,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 11,
+        "Address": "2600 Taylorsville Blvd, Salt Lake City, UT 84118",
+        "Delivery Deadline": "",
+        "Weight KILO": 1,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 12,
+        "Address": "3575 W Valley Central Station Bus Loop, West Valley City, UT 84119",
+        "Delivery Deadline": "",
+        "Weight KILO": 1,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 13,
+        "Address": "2010 W 500 S, Salt Lake City, UT 84104",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 2,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": True
+    },
+    {
+        "Package ID": 14,
+        "Address": "4300 S 1300 E, Millcreek, UT 84117",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 88,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": True
+    },
+    {
+        "Package ID": 15,
+        "Address": "4580 S 2300 E, Holladay, UT 84117",
+        "Delivery Deadline": "9:00",
+        "Weight KILO": 4,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": True
+    },
+    {
+        "Package ID": 16,
+        "Address": "4580 S 2300 E, Holladay, UT 84117",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 88,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": True
+    },
+    {
+        "Package ID": 17,
+        "Address": "3148 S 1100 W, Salt Lake City, UT 84119",
+        "Delivery Deadline": "",
+        "Weight KILO": 2,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 18,
+        "Address": "1488 4800 S, Salt Lake City, UT 84123",
+        "Delivery Deadline": "",
+        "Weight KILO": 6,
+        "Truck Number": "2",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 19,
+        "Address": "177 W Price Ave, Salt Lake City, UT 84115",
+        "Delivery Deadline": "",
+        "Weight KILO": 37,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": True
+    },
+    {
+        "Package ID": 20,
+        "Address": "3595 Main St, Salt Lake City, UT 84115",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 37,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": True
+    },
+    {
+        "Package ID": 21,
+        "Address": "3595 Main St, Salt Lake City, UT 84115",
+        "Delivery Deadline": "",
+        "Weight KILO": 3,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 22,
+        "Address": "6351 South 900 East, Murray, UT 84121",
+        "Delivery Deadline": "",
+        "Weight KILO": 2,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 23,
+        "Address": "5100 South 2700 West, Salt Lake City, UT 84118",
+        "Delivery Deadline": "",
+        "Weight KILO": 5,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 24,
+        "Address": "5025 State St, Murray, UT 84107",
+        "Delivery Deadline": "",
+        "Weight KILO": 7,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 25,
+        "Address": "5383 South 900 East #104, Salt Lake City, UT 84117",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 7,
+        "Truck Number": "",
+        "Delayed": True,
+        "Delayed Until": "9:05",
+        "Group": False
+    },
+    {
+        "Package ID": 26,
+        "Address": "5383 South 900 East #104, Salt Lake City, UT 84117",
+        "Delivery Deadline": "",
+        "Weight KILO": 25,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 27,
+        "Address": "1060 Dalton Ave S, Salt Lake City, UT 84104",
+        "Delivery Deadline": "",
+        "Weight KILO": 5,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 28,
+        "Address": "2835 Main St, Salt Lake City, UT 84115",
+        "Delivery Deadline": "",
+        "Weight KILO": 7,
+        "Truck Number": "",
+        "Delayed": True,
+        "Delayed Until": "9:05",
+        "Group": False
+    },
+    {
+        "Package ID": 29,
+        "Address": "1330 2100 S, Salt Lake City, UT 84106",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 2,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 30,
+        "Address": "300 State St, Salt Lake City, UT 84103",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 1,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 31,
+        "Address": "3365 S 900 W, Salt Lake City, UT 84119",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 1,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 32,
+        "Address": "3365 S 900 W, Salt Lake City, UT 84119",
+        "Delivery Deadline": "",
+        "Weight KILO": 1,
+        "Truck Number": "",
+        "Delayed": True,
+        "Delayed Until": "9:05",
+        "Group": False
+    },
+    {
+        "Package ID": 33,
+        "Address": "2530 S 500 E, Salt Lake City, UT 84106",
+        "Delivery Deadline": "",
+        "Weight KILO": 1,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 34,
+        "Address": "4580 S 2300 E, Holladay, UT 84117",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 2,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 35,
+        "Address": "1060 Dalton Ave S, Salt Lake City, UT 84104",
+        "Delivery Deadline": "",
+        "Weight KILO": 88,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 36,
+        "Address": "2300 Parkway Blvd, West Valley City, UT 84119",
+        "Delivery Deadline": "",
+        "Weight KILO": 88,
+        "Truck Number": "2",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 37,
+        "Address": "410 S State St, Salt Lake City, UT 84111",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 2,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 38,
+        "Address": "410 S State St, Salt Lake City, UT 84111",
+        "Delivery Deadline": "",
+        "Weight KILO": 9,
+        "Truck Number": "2",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 39,
+        "Address": "2010 W 500 S, Salt Lake City, UT 84104",
+        "Delivery Deadline": "",
+        "Weight KILO": 9,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    },
+    {
+        "Package ID": 40,
+        "Address": "380 W 2880 S, Salt Lake City, UT 84115",
+        "Delivery Deadline": "10:30",
+        "Weight KILO": 45,
+        "Truck Number": "",
+        "Delayed": False,
+        "Delayed Until": "",
+        "Group": False
+    }
+]
